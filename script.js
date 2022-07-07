6dbfdffb1fad656f332170c408327a7fb5c5801d
Shyam3050 <nimmanashyam3050@gmail.com>
2022-07-06 23:23:33 -1200

    responsive nav added

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-const menu = document.querySelector(".nav-toggle");
-const nav_links = document.querySelector(".nav-links");
-const nav_buttom = document.querySelector(".nav-buttom");
-menu.addEventListener("click", () => {
+// this is for adding active class to links
+function activeLinks(){
     nav_links.classList.toggle("active");
     nav_buttom.classList.toggle("active");
+}
+// bar closer
+function barCloser() {
+    if (toogleClick == 0) {
+      menuIcon.classList = "fa fa-times icon";
+      menuText.textContent= "Close";
+      return (toogleClick = 1);
+    }
+    if (toogleClick == 1) {
+      menuIcon.classList = "fa fa-bars icon";
+      menuText.textContent = "Menu";
+      return (toogleClick = 0);
+    }
+  }
+const menu = document.querySelector(".nav-toggle");
+const menuIcon = document.querySelector(".menu-icon");
+const menuText = document.querySelector(".menu-text");
+const nav_links = document.querySelector(".nav-links");
+const nav_buttom = document.querySelector(".nav-buttom");
+const allLinks = [...document.querySelectorAll(".nav-link"), ...document.querySelectorAll(".nav-buttom-links")];
+let toogleClick = 0;
+menu.addEventListener("click", () => {
+    activeLinks();
+    barCloser();
 })
-console.log(nav_buttom);
+allLinks.forEach((link) => {
+    link.addEventListener("click",()=>{
+        activeLinks();
+        barCloser();
+    })
+})
